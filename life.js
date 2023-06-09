13a21d5c49fa3339a3af8d9635d14b29d55776ac
scifisatan <brawlershere@gmail.com>
2023-06-10 01:08:30 +0545

    working code

--- life.js
+++ life.js
@@ -1,5 +1,3 @@
-// import {state} from './blackbox.js';
-
 canvas = document.getElementById("life");
 
 canvas.width = window.innerWidth;
@@ -26,70 +24,6 @@ let val = {
   "outerRadius":12,
 }
 
-
-// function generateHexColor(value) {
-//   const hue = Math.floor(value * 360);
-//   const saturation = 100;
-//   const lightness = 50;
-
-//   // Convert HSL to RGB
-//   const hslToRgb = (h, s, l) => {
-//     h /= 360;
-//     s /= 100;
-//     l /= 100;
-  
-//     let r, g, b;
-  
-//     if (s === 0) {
-//       r = g = b = l; // achromatic
-//     } else {
-//       const hue2rgb = (p, q, t) => {
-//         if (t < 0) t += 1;
-//         if (t > 1) t -= 1;
-//         if (t < 1 / 6) return p + (q - p) * 6 * t;
-//         if (t < 1 / 2) return q;
-//         if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
-//         return p;
-//       };
-  
-//       const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-//       const p = 2 * l - q;
-  
-//       r = hue2rgb(p, q, h + 1 / 3);
-//       g = hue2rgb(p, q, h);
-//       b = hue2rgb(p, q, h - 1 / 3);
-//     }
-  
-//     return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
-//   };
-
-//   // Convert RGB to hex
-//   const rgbToHex = (r, g, b) => {
-//     const toHex = (c) => {
-//       const hex = c.toString(16);
-//       return hex.length === 1 ? '0' + hex : hex;
-//     };
-
-//     return `#${toHex(r)}${toHex(g)}${toHex(b)}`;
-//   };
-
-//   // Convert HSL to RGB and then to hex
-//   const [r, g, b] = hslToRgb(hue, saturation, lightness);
-//   const hexColor = rgbToHex(r, g, b);
-
-//   return hexColor;
-// }
-
-
-// let render = (matrix) => {
-//   for (let i = 0; i < HEIGHT; i++) {
-//     for(let j = 0; j <= WIDTH; j++) {
-//       color = generateHexColor(matrix[i][j]);
-//       ctx.fillStyle = color;
-//       ctx.fillRect(j * cellWidth, i * cellHeight, cellWidth, cellHeight);
-//     }
-//   }
-// }
 
 let render = (matrix) => {
   for (let i = 0; i < HEIGHT; i++) {
@@ -172,27 +106,18 @@ const update = (grid,nextGrid) => {
 
       n = res[0];
       m = res[1];
-      // console.log(n,m)
-      // console.log(state(n, m))
-      // n = a-m;
+ 
       nextGrid[i][j] = state(n, m);
-      // nextGrid[i][j] = 1-grid[i][j];
+     
     }
   }
 }
 
-// render(grid);
 
 canvas.addEventListener("click", (e) => {
   update(grid,nextGrid);
   [grid, nextGrid] = [nextGrid, grid];
   render(grid);
-  // res = new Array(2);
-  // x = Math.floor(e.offsetX/cellWidth);
-  // y = Math.floor(e.offsetY/cellHeight);
-  // res[1] = calculateMean(grid, x, y, val["outerRadius"]);
-  // console.log(res[0], res[1])
-
 });
 
 setInterval(() => {
