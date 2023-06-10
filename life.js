0ef709d93f2c3fba1c823073d092959888040152
scifisatan <brawlershere@gmail.com>
2023-06-11 02:00:51 +0545

    working more now

--- life.js
+++ life.js
@@ -11,121 +11,152 @@ HEIGHT = Math.floor(canvas.height / cellHeight);
 
 const ctx = canvas.getContext("2d", { alpha: false });
 
-let grid = new Array(HEIGHT).fill(0).map(row => new Array(WIDTH).fill(0).map(cell => Math.random() ));
-let nextGrid = new Array(HEIGHT).fill(0).map(row => new Array(WIDTH).fill(0).map(cell => Math.random() ));
+
 
 let val = {
-  "b1": 0.002,
-  "b2": 0.9,
-  "d1": 0.45,
-  "d2": 0.87,
-  "alpha": 0.178,
-  "innerRadius":2,
-  "outerRadius":12,
+  "b1": 0.278,
+  "b2": 0.365,
+  "d1": 0.267,
+  "d2": 0.445,
+  "dt": 0.07,
+  "alphaM": 0.147,
+  "alphaN": 0.028,
+  "innerRadius": 7,
+  "outerRadius": 21
 }
 
+let grid = new Array(HEIGHT).fill(0).map(row => new Array(WIDTH).fill(0).map(cell => Math.random()));
+let nextGrid = new Array(HEIGHT).fill(0).map(row => new Array(WIDTH).fill(0).map(cell => Math.random()));
 
 let render = (matrix) => {
   for (let i = 0; i < HEIGHT; i++) {
-    for(let j = 0; j <= WIDTH; j++) {
+    for (let j = 0; j <= WIDTH; j++) {
       color = Math.floor(matrix[i][j] * 255);
-      ctx.fillStyle = 'rgb('+color+','+color+','+color+')';
+      ctx.fillStyle = 'rgb(' + color + ',' + color + ',' + color + ')';
       ctx.fillRect(j * cellWidth, i * cellHeight, cellWidth, cellHeight);
     }
   }
 }
 
-const sigma1 = (x, a) => {
-  return 1 / (1 + Math.exp(-(x - a) * 4 /val["alpha"]));
+const emod = (x, y) => {
+  return (x % y + y) % y;
 }
 
-const sigma2 = (x, a, b) => {
-  return sigma1(x, a) * (1 - sigma1(x, b));
+const sigma = (x, a, alpha) => {
+  return 1 / (1 + Math.exp(-(x - a) * 4 / alpha));
+}
+
+const sigmaN = (x, a, b) => {
+  return sigma(x, a, val["alphaN"]) * (1 - sigma(x, b, val["alphaN"]));
 }
 
 const sigmaM = (x, y, m) => {
-  return x * (1 - sigma1(m, 0.5)) + y * sigma1(m, 0.5);
+  return x * (1 - sigma(m, 0.5, val["alphaM"])) + y * sigma(m, 0.5, val["alphaM"]);
 }
 
 const state = (n, m) => {
-  return sigma2(
+  return sigmaN(
     n,
     sigmaM(
       val["b1"],
       val["d1"],
       m),
     sigmaM(
-      val["b2"], 
-      val["d2"], 
+      val["b2"],
+      val["d2"],
       m
     )
-   );
+  );
 }
 
-const emod = (x, y) => {
-  return (x%y + y)%y;
-}
 
-const calculateMean = (array, centerX, centerY, radius) => {
-  ri = radius/3;
-  m = n = M = N = 0;
-  for (let i = -(radius-1); i <= (radius - 1); ++i) {
-  for (let j = -(radius-1) ; j <= (radius-1); ++j) {
-      if (i >= 0 && i < array.length && j >= 0 && j < array[0].length) {
-          x = emod(j + centerX , WIDTH);
-          y = emod(i + centerY , HEIGHT);
+const update = () => {
+  for (let cx = 0; cx < WIDTH; cx++) {
+    for (let cy = 0; cy < HEIGHT; cy++) {
+      let m = n = M = N = 0;
+      ra = val["outerRadius"];
+      ri = ra / 3.0;
+      m = n = M = N = 0;
+      for (let dy = -(ra - 1); dy <= (ra - 1); ++dy) {
+        for (let dx = -(ra - 1); dx <= (ra - 1); ++dx) {
+          x = emod(cx + dx, WIDTH);
+          y = emod(cy + dy, HEIGHT);
 
-          if(i*i + j*j <= ri*ri) {
-            m += array[y][x];
+          if (dx * dx + dy * dy <= ri * ri) {
+            m += grid[y][x];
             M++;
           }
-          else if(i*i + j*j <= radius*radius) {
-            n += array[y][x];
+          else if (dx * dx + dy * dy <= ra * ra) {
+            n += grid[y][x];
             N++;
           }
 
-          }
+        }
 
 
       }
+      m /= M;
+      n /= N;
+      nextGrid[cy][cx] = 2 * state(n, m) - 1;
     }
 
-    return [n/N, m/M] ;
   }
+}
 
+const clamp = (x, min, max) => {
+  return x < min ? min : x > max ? max : x;
+}
 
-
-
-const update = (grid,nextGrid) => {
-  res = new Array(2);
-  m = n = 0;
+const apply = () => {
   for (let i = 0; i < HEIGHT; i++) {
-    for(let j = 0; j <= WIDTH; j++) {
-      res = calculateMean(grid, i, j, val["outerRadius"]);
-  
+    for (let j = 0; j <= WIDTH; j++) {
+      grid[i][j] += val["dt"] * nextGrid[i][j];
+      grid[i][j] = clamp(grid[i][j], 0, 1);
 
-      n = res[0];
-      m = res[1];
- 
-      nextGrid[i][j] = state(n, m);
-     
     }
   }
 }
 
 
-canvas.addEventListener("click", (e) => {
-  update(grid,nextGrid);
-  [grid, nextGrid] = [nextGrid, grid];
-  render(grid);
-});
+
 
 setInterval(() => {
-  update(grid,nextGrid);
-  [grid, nextGrid] = [nextGrid, grid];
+
+  update(grid, nextGrid);
+  apply(grid, nextGrid);
   render(grid);
+
+
 }
-, 1000/2);
+  , 1000 / 50);
+
+
+//play pause and reset button use
+// let play = document.getElementById("play");
+// let pause = document.getElementById("pause");
+// let reset = document.getElementById("reset");
+
+// play.addEventListener("click", () => {
+//   setInterval(() => {
+//     update(grid,nextGrid);
+//     [grid, nextGrid] = [nextGrid, grid];
+//     render(grid);
+//   }
+//   , 2000);
+// }
+// );
+
+// pause.addEventListener("click", () => {
+//   clearInterval();
+// }
+// );
+
+// reset.addEventListener("click", () => {
+//   grid = new Array(HEIGHT).fill(0).map(row => new Array(WIDTH).fill(0).map(cell => Math.random() ));
+//   nextGrid = new Array(HEIGHT).fill(0).map(row => new Array(WIDTH).fill(0).map(cell => Math.random() ));
+//   render(grid);
+// }
+// );
 
 
 
@@ -134,10 +165,11 @@ setInterval(() => {
 
 
 
-      
-  
 
-    
+
+
+
+
 
 
 
